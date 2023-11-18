f9068aaba1dc2ce135cdaa2441d8f84bbe37570f
James <lwinnyihtut0@gmail.com>
2023-11-18 19:49:27 +0630

    changes code

--- scripts/load-fake-data.ts
+++ scripts/load-fake-data.ts
@@ -5,12 +5,12 @@ const projectDir = process.cwd();
 loadEnvConfig(projectDir);
 
 async function loadFakeData(numUser: number = 10) {
-  console.log(`executing load fake data. generating ${numUser} users`);
-  console.log(process.env.POSTGRES_USER);
-  console.log(process.env.POSTGRES_HOST);
-  console.log(process.env.POSTGRES_NAME);
-  console.log(process.env.POSTGRES_PASSWORD);
-  console.log(parseInt(process.env.POSTGRES_PORT!));
+  // console.log(`executing load fake data. generating ${numUser} users`);
+  // console.log(process.env.POSTGRES_USER);
+  // console.log(process.env.POSTGRES_HOST);
+  // console.log(process.env.POSTGRES_NAME);
+  // console.log(process.env.POSTGRES_PASSWORD);
+  // console.log(parseInt(process.env.POSTGRES_PORT!));
   const client = new Client({
     user: process.env.POSTGRES_USER,
     host: process.env.POSTGRES_HOST,
@@ -18,19 +18,21 @@ async function loadFakeData(numUser: number = 10) {
     password: process.env.POSTGRES_PASSWORD,
     port: parseInt(process.env.POSTGRES_PORT!),
   });
-  //   const res = await client.query("select 1");
-  //   console.log(res);
-  try {
-    await client.connect();
-    console.log("Connected to the database");
+  await client.connect();
+  const res = await client.query("select 1");
+  console.log(res);
+  await client.end();
+  // try {
+  //   await client.connect();
+  //   console.log("Connected to the database");
 
-    const res = await client.query("SELECT 1");
-    console.log(res);
-  } catch (error) {
-    console.error("Error connecting to the database:", error.message);
-  } finally {
-    await client.end();
-  }
+  //   const res = await client.query("SELECT 1");
+  //   console.log(res);
+  // } catch (error) {
+  //   console.error("Error connecting to the database:", error.message);
+  // } finally {
+  //   await client.end();
+  // }
 }
 
 loadFakeData();
