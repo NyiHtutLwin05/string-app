da147cda485eef452ba50829475decec52798548
James <lwinnyihtut0@gmail.com>
2023-11-28 11:31:11 +0630

    private layout shell

---
+++ scripts/pratice.js
@@ -0,0 +1,9 @@
+var hero;
+function addTwo(num) {
+    return num + 2;
+}
+addTwo(5);
+var FunctionAdd = function (s) {
+    return s;
+};
+FunctionAdd("apt");
--- scripts/pratice.ts
+++ scripts/pratice.ts
@@ -5,3 +5,15 @@ function addTwo(num: number) {
 }
 
 addTwo(5);
+
+const FunctionAdd = (s: string): string => {
+  console.log(s);
+};
+
+FunctionAdd("apt");
+
+const heros = ["Thor", "Superman", "IronMan"];
+heros.map((h) => {
+  return `hero is ${h}`;
+});
+export {};
